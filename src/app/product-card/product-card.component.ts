75072daec8d9f09380288cb2827e1e13aa55c138
Zzxz <howie951018@gmail.com>
2026-04-01 18:12:47 +0800

    feat(product): 利用文字插值顯示產品名稱、作者與公司資訊

---
+++ src/app/product-card/product-card.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-card',
+  imports: [],
+  templateUrl: './product-card.component.html',
+  styleUrl: './product-card.component.scss',
+})
+export class ProductCardComponent {
+  protected productName = '書籍 A';
+  protected author = '作者甲、作者乙、作者丙';
+  protected company = '博碩文化';
+}
