b8f2888f6ed631e4a580b236e69b9fc027c2a97a
Zzxz <howie951018@gmail.com>
2026-05-13 18:08:31 +0800

    feat(product): 變更產品編號型別為字串

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -11,7 +11,7 @@ export class ProductService {
     private _data: Product[] = [
 
       new Product({
-        id: 1,
+        id: '1',
         name: '書籍 A',
         authors: ['作者甲', '作者乙', '作者丙'],
         company: '博碩文化',
@@ -21,7 +21,7 @@ export class ProductService {
         price: 10000,
       }),
       new Product({
-        id: 2,
+        id: '2',
         name: '書籍 B',
         authors: ['作者甲', '作者乙', '作者丙'],
         company: '博碩文化',
@@ -31,7 +31,7 @@ export class ProductService {
         price: 10000,
       }),
       new Product({
-        id: 3,
+        id: '3',
         name: '書籍 C',
         authors: ['作者甲', '作者乙', '作者丙'],
         company: '博碩文化',
@@ -41,7 +41,7 @@ export class ProductService {
         price: 10000,
       }),
       new Product({
-        id: 4,
+        id: '4',
         name: '書籍 D',
         authors: ['作者甲', '作者乙', '作者丙'],
         company: '博碩文化',
@@ -51,7 +51,7 @@ export class ProductService {
         price: 10000,
       }),
       new Product({
-        id: 5,
+        id: '5',
         name: '書籍 E',
         authors: ['作者甲', '作者乙', '作者丙'],
         company: '博碩文化',
@@ -62,7 +62,7 @@ export class ProductService {
       }),
     ];
 
-  getById(productId: number): Observable<Product> {
+  getById(productId: string): Observable<Product> {
     return of(this._data).pipe(
       mergeMap((data) => data),
       filter(({ id }) => id === productId)
@@ -85,13 +85,13 @@ export class ProductService {
   }
 
   add(product: Readonly<Product>): Observable<Product> {
-    const id = this._data.length === 0 ? 1 : Math.max(...this._data.map(({ id }) => id)) + 1;
-    const newProduct = new Product({ ...product, id });
+    const id = this._data.length === 0 ? 1 : Math.max(...this._data.map(({ id }) => +id)) + 1;
+    const newProduct = new Product({ ...product, id: id.toString() });
     this._data.push(newProduct);
     return of(newProduct);
   }
 
-  remove(productId: number): Observable<Product> {
+  remove(productId: string): Observable<Product> {
     const index = this._data.findIndex(({ id }) => id === productId);
     const [product] = this._data.splice(index, 1);
     return of(product);
